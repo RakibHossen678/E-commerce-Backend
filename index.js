bf99450d7142e155b3acd42af59009c6366352ba
RakibHossen678 <rh491464@gmail.com>
2024-09-18 17:39:21 +0600

    mongoDB setup done

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+
+main()
+  .then(() => console.log("MongoDB is Successfully connected"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrdje6l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  );
+}
 
 app.get("/", (req, res) => {
   res.send("Hello from StyleBask server....!");
